254e0dac60efe568d180a8c4a60b816a55fdb474
Rod Johnson <johnsonroda@gmail.com>
2018-11-19 14:33:40 +0100

    Initial commit from Atomist

---
+++ src/main/java/ax3/Ax3Application.java
@@ -0,0 +1,12 @@
+package ax3;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ax3Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(Ax3Application.class, args);
+	}
+
+}
